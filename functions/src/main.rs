3a2e266347cac6971d00a6b42f5ff3ae9b7a281f
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-22 10:42:26 -0600

    define parameter

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
