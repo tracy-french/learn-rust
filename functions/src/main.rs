8610bd3869bd7e1137e3c6727fcc0de1afb90404
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-22 10:45:06 -0600

    multiple parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
