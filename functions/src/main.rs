0cc95c67724739886995d9ce5f28dfa327fcee7c
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-22 10:51:02 -0600

    expression function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,9 @@
-fn main() {
-    print_labeled_measurement(5, 'h');
+fn five() -> i32 {
+    5
 }
 
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
