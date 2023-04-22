4b37aff4370b8c0baf8b23a13663441cdbcb7732
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-22 11:02:04 -0600

    if requires bool

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number != 0 {
+        println!("number was something other than zero");
     }
 }
