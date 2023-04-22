1f42d287830af463a4089e2c9e451451a480260c
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-22 11:28:28 -0600

    while loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,27 +1,11 @@
 fn main() {
-    let mut count = 0;
+    let mut number = 3;
 
-    'counting_up: loop {
-        println!("count = {count}");
-        
-        let mut remaining = 10;
-
-        loop {
-            println!("remaining = {remaining}");
-
-            if remaining == 9 {
-                break;
-            }
-
-            if count == 2 {
-                break 'counting_up;
-            }
-
-            remaining -= 1;
-        }
-
-        count += 1;
+    while number != 0 {
+        println!("{number}!");
+    
+        number -= 1;
     }
 
-    println!("End count = {count}");
+    println!("LIFTOFF!!!");
 }
