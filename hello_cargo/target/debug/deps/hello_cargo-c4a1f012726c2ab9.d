d6ba8737d3fa9eb7c6abae3aed16eeb6afed54b9
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-21 20:26:30 -0600

    remove hello_cargo target

--- hello_cargo/target/debug/deps/hello_cargo-c4a1f012726c2ab9.d
+++
@@ -1,5 +0,0 @@
-/Users/tracyfrench/dev/rust/hello_cargo/target/debug/deps/hello_cargo-c4a1f012726c2ab9: src/main.rs
-
-/Users/tracyfrench/dev/rust/hello_cargo/target/debug/deps/hello_cargo-c4a1f012726c2ab9.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/tracyfrench/dev/rust/hello_cargo/target/debug/hello_cargo: /Users/tracyfrench/dev/rust/hello_cargo/src/main.rs
